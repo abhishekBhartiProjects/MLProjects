e75654d61f6fae58b02ea7f3813a3cbbf8389de6
abhishekbharti <abhishek.bharti@testbook.com>
2018-10-04 02:15:14 +0530

    libraries imported

--- iris_dataset.py
+++ iris_dataset.py
@@ -3,44 +3,44 @@
 #check the version of libraries
 
 
-#python version
-import sys
-print('Python: {}'.format(sys.version))
+# #python version
+# import sys
+# print('Python: {}'.format(sys.version))
 
-#scipy
-import scipy
-print('scipy: {}'.format(scipy.__version__))
+# #scipy
+# import scipy
+# print('scipy: {}'.format(scipy.__version__))
 
-#numpy
-import numpy
-print('numpy: {}'.format(numpy.__version__))
+# #numpy
+# import numpy
+# print('numpy: {}'.format(numpy.__version__))
 
-#matplotlib
-import matplotlib
-print('matplotlib: {}'.format(matplotlib.__version__))
+# #matplotlib
+# import matplotlib
+# print('matplotlib: {}'.format(matplotlib.__version__))
 
-#pandas
-import pandas
-print('pandas: {}'.format(pandas.__version__))
+# #pandas
+# import pandas
+# print('pandas: {}'.format(pandas.__version__))
 
-#scikit-learn
-import sklearn
-print('sklearn: {}'.format(sklearn.__version__))
+# #scikit-learn
+# import sklearn
+# print('sklearn: {}'.format(sklearn.__version__))
 
 # load libraries
-# import pandas
-# from pandas.plotting import scatter_matrix
-# import matplotlib.pyplot as plt 
-# from sklearn import model_selection
-# from sklearn.metrics import classification_report
-# from sklearn.metrics import confusion_matrix
-# from sklearn.metrics import accuracy_score
-# from sklearn.linear_model import LogisticRegression
-# from sklearn.tree import DecisionTreeClassifier
-# from sklearn.neighbors import KNeighborsClassifier
-# from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-# from sklearn.naive_bayes import GaussianNB
-# from sklearn.svm import SVC
+import pandas
+from pandas.plotting import scatter_matrix
+import matplotlib.pyplot as plt 
+from sklearn import model_selection
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
 
 # #data
 # url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
