c051a804a284d2f510b0368d943df75df0211c9a
abhishekbharti <abhishek.bharti@testbook.com>
2018-10-04 00:06:56 +0530

    comment

--- classification.py
+++ classification.py
@@ -4,4 +4,6 @@ label = [0, 0, 1, 1]
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(feature, label)
 
+
+#this function will predict that (160, 0) will be classified into group having label = 1
 print clf.predict([[160, 0]])
